bfee919899fdb0e341032930944cfaef9855989c
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-06-05 14:25:04 -0400

    Update file.py

--- src/loggingtoolspy/file.py
+++ src/loggingtoolspy/file.py
@@ -12,10 +12,12 @@ def logs_file_setup(file: str, level=logging.INFO, root_logs_dir: str = "./"):
     logs_dir = f"{root_logs_dir}/logs/logs-{today.strftime('%d-%m-%Y')}"
     logs_file = f'{logs_dir}/{os.path.splitext(os.path.basename(file))[0]}-{timestamp}.log'
     os.makedirs(logs_dir, exist_ok=True)
-    logging.basicConfig(filename=logs_file, filemode='w+', level=level)
+    logging.basicConfig(level=level)
+    logging.getLogger().addHandler(logging.FileHandler(logs_file))
     sh = logging.StreamHandler(sys.stdout)
     logging.getLogger().addHandler(sh)
     logging.info(f"Logs file at: {logs_file}\n")
+    return logs_file
 
 
 
