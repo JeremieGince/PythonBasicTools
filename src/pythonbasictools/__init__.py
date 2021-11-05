b562e209fceebad55c51a9339821bbe4d7e52d92
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-11-04 22:20:00 -0400

    Update __init__.py

--- src/pythonbasictools/__init__.py
+++ src/pythonbasictools/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Jérémie Gince"
 
-from .file import logs_file_setup
+from .logging import logs_file_setup
 from .device import DeepLib, log_device_setup
 from .decorators import log_func
 from .multiprocessing import worker_init, multiprocess_logger_init
