7bfa7231e901322aba5c2ec0efcc22aed7116124
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-02-22 14:05:21 -0500

    add args to log_func

--- src/pythonbasictools/decorators.py
+++ src/pythonbasictools/decorators.py
@@ -1,18 +1,26 @@
+import functools
 import logging
 import time
 
 
-def log_func(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        logging.info(f"\n--- {func.__name__} -> begin --- \n")
-        out = func(*args, **kwargs)
-        logging.info(f"\n--- {func.__name__} -> end in {time.time() - start_time:.2}s ---\n")
-        return out
-
-    wrapper.__name__ = func.__name__ + "@log_func"
-    return wrapper
-
-
-
+def log_func(_func=None, *, box_length=50, box_char='-', logging_func=logging.info):
+    def decorator_log_func(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            head = (box_char*box_length) + ' ' + func.__name__ + ' ' + (box_char*box_length)
+            logging_func(head)
+            out = func(*args, **kwargs)
+            elapsed_time_msg = f"<---> Elapsed time {time.time() - start_time:.2}s <--->"
+            logging_func(elapsed_time_msg)
+            logging_func(box_char*len(head))
+            return out
+    
+        wrapper.__name__ = func.__name__ + "@log_func"
+        return wrapper
+    
+    if _func is None:
+        return decorator_log_func
+    else:
+        return decorator_log_func(_func)
 
