af358ded595a39123898cd73710f7a255706e764
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-08 16:39:02 -0400

    Update __init__.py

--- src/pythonbasictools/__init__.py
+++ src/pythonbasictools/__init__.py
@@ -10,3 +10,4 @@ from .device import DeepLib, log_device_setup
 from .decorators import log_func
 from .multiprocessing import worker_init, multiprocess_logger_init
 from .slurm import SlurmHostServer, send_slurm_cmd, generate_slurm_cmd
+from . import docstring
