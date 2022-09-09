1ce684b456aa6ca3d2d1310af8de0650ea53624e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-08 20:22:37 -0400

    Update conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -73,7 +73,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.githubpages',
-    'sphinx_rtd_theme',
+    'groundwork',
     'sphinxcontrib.bibtex',
 ]
 
@@ -86,5 +86,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'groundwork'
 html_static_path = ['_static']
