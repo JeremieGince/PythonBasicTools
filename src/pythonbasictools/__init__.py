4d614249be23903584a088c2500c670b4b226b6e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2025-01-19 15:25:20 -0500

    Update __init__.py

--- src/pythonbasictools/__init__.py
+++ src/pythonbasictools/__init__.py
@@ -3,7 +3,7 @@ __email__ = "gincejeremie@gmail.com"
 __copyright__ = "Copyright 2021, Jérémie Gince"
 __license__ = "Apache 2.0"
 __url__ = "https://github.com/JeremieGince/PythonBasicTools"
-__version__ = "0.0.1-alpha.10"
+__version__ = "0.0.1-alpha.11"
 
 from .logging_tools import logs_file_setup
 from .device import DeepLib, log_device_setup
