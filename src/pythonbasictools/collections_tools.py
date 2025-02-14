ce76f2ef2172bb9bfe0647e6fb92c8f5e61cd8cb
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2025-02-14 15:43:32 -0500

    Update collections_tools.py

--- src/pythonbasictools/collections_tools.py
+++ src/pythonbasictools/collections_tools.py
@@ -1,3 +1,6 @@
+from typing import Sequence, Any, List
+
+
 def ravel_dict(d: dict, key_sep: str = ".") -> dict:
     """
     Ravel a dictionary into a single level dictionary.
@@ -18,3 +21,49 @@ def ravel_dict(d: dict, key_sep: str = ".") -> dict:
         else:
             raveled_dict[k] = v
     return raveled_dict
+
+
+def sequence_get(sequence: Sequence, idx: int, default: Any = None) -> Any:
+    """
+    Get an item from a sequence at a specific index. If the index is out of bounds, return a default value.
+
+    :param sequence: The sequence to get the item from.
+    :type sequence: Sequence
+    :param idx: The index to get the item from.
+    :type idx: int
+    :param default: The default value to return if the index is out of bounds.
+    :type default: Any
+
+    :return: The item at the index or the default value.
+    :rtype: Any
+    """
+    try:
+        return sequence[idx]
+    except IndexError:
+        return default
+
+
+def list_insert_replace_at(__list: List, idx: int, value: Any, default: Any = None) -> List:
+    """
+    Insert a value at a specific index.
+    If there is already a value at this index, replace it.
+
+    :param __list: The list to modify.
+    :type __list: List
+    :param idx: The index to insert the value.
+    :type idx: int
+    :param value: The value to insert.
+    :type value: Any
+    :param default: The default value to insert if the index is out of bounds.
+    :type default: Any
+
+    :return: The new list.
+    :rtype: List
+    """
+    if idx < len(__list):
+        __list[idx] = value
+    else:
+        __list.extend([default] * (idx - len(__list)))
+        __list.append(value)
+    return __list
+
